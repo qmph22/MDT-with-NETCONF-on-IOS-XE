e3e2fbb7d3d11ce62e37175ba52319dae952c6e4
Quang Phan <qp@home.net>
2025-05-15 19:07:56 -0400

    Added except block for the initial wrong password

--- telegraf/netconf-cpu.py
+++ telegraf/netconf-cpu.py
@@ -8,7 +8,7 @@ def dict_to_telegraf_json(rpc_reply_dict: Dict) -> str:
     """
 
     stats_array = []
-    
+
     for item, value in rpc_reply_dict["rpc-reply"]["data"]["cpu-usage"]["cpu-utilization"].items():
         dict = {
             "name": "CPU",
@@ -22,16 +22,7 @@ def dict_to_telegraf_json(rpc_reply_dict: Dict) -> str:
 
 
 def main():
-    with manager.connect(
-        host = "10.0.0.1",  # Locally connected device. Will eventually make this a configurable item
-        port = 830,
-        username = "admin",
-        password = "admin",  # <------- enter device password
-        hostkey_verify=False,
-        device_params = {'name': 'iosxe'}
-    ) as m:
-        # https://github.com/YangModels/yang/blob/master/vendor/cisco/xe/16111/Cisco-IOS-XE-process-memory-oper.yang
-        netconf_filter = """
+    netconf_filter = """
         <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
             <cpu-usage xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-process-cpu-oper">
                 <cpu-utilization>
@@ -41,11 +32,30 @@ def main():
             </cpu-usage>
         </filter>
         """
-
-        netconf_rpc_reply = m.get(
-            filter = netconf_filter
-        ).xml
-
+    try:
+        with manager.connect(
+            host = "10.24.0.100",  # Locally connected device. Will eventually make this a configurable item
+            port = 830,
+            username = "admin",
+            password = "admin",  # <------- enter device password
+            hostkey_verify=False,
+            device_params = {'name': 'iosxe'}
+        ) as m:
+            netconf_rpc_reply = m.get(
+                filter = netconf_filter
+            ).xml
+    except:
+        with manager.connect(
+            host = "10.24.0.100",  # Locally connected device. Will eventually make this a configurable item
+            port = 830,
+            username = "admin",
+            password = "admin2",  # <------- enter device password
+            hostkey_verify=False,
+            device_params = {'name': 'iosxe'}
+        ) as m:
+            netconf_rpc_reply = m.get(
+                filter = netconf_filter
+            ).xml
         netconf_reply_dict = xmltodict.parse(netconf_rpc_reply)
 
         telegraf_json_input = dict_to_telegraf_json(netconf_reply_dict)
