f12987a6c9308566b77ebd3b8706f2f6ced0a83e
Quang Phan <qmphan22@gmail.com>
2025-05-16 21:31:49 -0400

    Added yml file for looping through credentials. Added env file to store passwords

--- telegraf/netconf-memory-pools.py
+++ telegraf/netconf-memory-pools.py
@@ -2,6 +2,9 @@ from typing import Dict
 from ncclient import manager
 import xmltodict
 import json
+from dotenv import load_dotenv
+import os
+import yaml
 
 def dict_to_telegraf_json(rpc_reply_dict: Dict) -> str:
     """
@@ -19,18 +22,14 @@ def dict_to_telegraf_json(rpc_reply_dict: Dict) -> str:
 
     return json.dumps(memory_stats_array)  # return JSON formatted data
 
-
 def main():
-    with manager.connect(
-        host = "10.0.0.1",  # Locally connected device. Will eventually make this a configurable item
-        port = 830,
-        username = "admin",
-        password = "admin",  # <--------- enter device password
-        hostkey_verify=False,
-        device_params = {'name': 'iosxe'}
-    ) as m:
-        # https://github.com/YangModels/yang/blob/master/vendor/cisco/xe/16111/Cisco-IOS-XE-memory-oper.yang
-        netconf_filter = """
+    load_dotenv()
+
+    with open('networkdevices.yml', 'r') as file:
+        config = yaml.safe_load(file)
+
+    # https://github.com/YangModels/yang/blob/master/vendor/cisco/xe/16111/Cisco-IOS-XE-memory-oper.yang
+    netconf_filter = """
         <filter xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
             <memory-statistics xmlns="http://cisco.com/ns/yang/Cisco-IOS-XE-memory-oper">
                 <memory-statistic>
@@ -40,20 +39,35 @@ def main():
                 </memory-statistic>
             </memory-statistics>
         </filter>
-        """
+        """        
 
-        netconf_rpc_reply = m.get(
-            filter = netconf_filter
-        ).xml
+    for deviceIndex in range(0, len(config['devices'])):
+        for credential in config['devices'][deviceIndex]['credentials']:
+            try:
+                with manager.connect(
+                    host = config['devices'][deviceIndex]['host'],  # Locally connected device. Will eventually make this a configurable item
+                    port = config['devices'][deviceIndex]['port'],
+                    username = credential['username'],
+                    password = os.getenv(credential['password_env']),  # <--------- enter device password
+                    hostkey_verify = bool(config['devices'][deviceIndex]['hostkey_verify']),
+                    device_params = config['devices'][deviceIndex]['device_params']
+                ) as m:
+                    netconf_rpc_reply = m.get(
+                        filter = netconf_filter
+                    ).xml
+                break
+            except:
+                if deviceIndex == len(config['devices']):
+                    exit(1)
+                else:
+                    continue
 
         netconf_reply_dict = xmltodict.parse(netconf_rpc_reply)
-
         telegraf_json_input = dict_to_telegraf_json(netconf_reply_dict)
 
         # telegraf needs data in a certain data format.
         # I have chosen JSON data that will be picked up by the exec plugin
         print(telegraf_json_input)
-
 
 if __name__ == "__main__":
     main()
